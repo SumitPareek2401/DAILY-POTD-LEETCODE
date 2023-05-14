155cc4fb35e5dbc249227f635a675f4d76e81898
SumitPareek2401 <122019171+SumitPareek2401@users.noreply.github.com>
2023-05-14 19:39:28 +0530

    Create 14 May | 1799. Maximize Score After N Operations.cpp

---
+++ 14 May | 1799. Maximize Score After N Operations.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int solve(vector<int>&nums, int mask, int countOfOp, vector<int> &dp){
+        int m = nums.size(), n = nums.size()/2;
+        if(countOfOp >= n){
+            return 0;
+        }
+        if(dp[mask] != -1){
+            return dp[mask];
+        }
+        int maxScore = 0;
+        for(int i = 0; i < m; ++i){
+            if(((mask >> i) & 1) == 1){
+                continue;
+            }
+            for(int j = i+1; j < m; ++j){
+                if(((mask >> j) & 1) == 1){
+                    continue;
+                }
+                int newMask = mask | (1 << i) | ((1 << j));
+
+                int currScore = (countOfOp + 1) * __gcd(nums[i], nums[j]);
+                int remainingScore = solve(nums, newMask, countOfOp+1, dp);
+                maxScore = max(maxScore, currScore+remainingScore);
+            }
+        }
+        return dp[mask] = maxScore;
+    }
+
+    int maxScore(vector<int>& nums) {
+        int m = nums.size();
+        int maxMaskValue = 1 << m;
+        vector<int> dp(maxMaskValue, -1);
+        return solve(nums, 0, 0, dp);
+    }
+};
